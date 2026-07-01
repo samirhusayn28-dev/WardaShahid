ab7267b5dc3af265e92f5f75b508320f2bb61284
Samir Husayn <samirhusayn28-dev@users.noreply.github.com>
2026-07-01 17:00:32 +0500

    Updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -150,7 +150,7 @@ const SOCIALS = [
   },
   {
     label: 'LinkedIn', username: '@Warda Shahid', delay: '2.0s',
-    url: 'https://www.linkedin.https://www.linkedin.com/in/warda-shahid-44090b405?utm_source=share_via&utm_content=profile&utm_medium=member_android/in/warda-shahid-44090b405?utm_source=share_via&utm_content=profile&utm_medium=member_android',
+    url: 'https://www.linkedin.com/in/warda-shahid-44090b405/',
     icon: (w, h) => <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" width={w} height={h}><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" /><rect x="2" y="9" width="4" height="12" /><circle cx="4" cy="4" r="2" /></svg>,
   },
   {
